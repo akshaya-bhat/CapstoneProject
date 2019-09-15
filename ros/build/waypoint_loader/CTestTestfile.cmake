d24b69f0a969ee49e3a7432f18475233db4fd2a9
KOKSANG <koksang1994@gmail.com>
2019-09-15 14:23:53 +0800

    cleanup

--- ros/build/waypoint_loader/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /udacity/ros/src/waypoint_loader
-# Build directory: /udacity/ros/build/waypoint_loader
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
